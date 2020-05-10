2350cef9de683d194a9674adeb1daa3b99d61fa5
Koddiak <lnevarez@protonmail.com>
2020-05-09 19:16:20 -0700

    Initial Commit
    Added the "basic_javascript_projects" folder and it's content. (added basic_html_1.html and basic_javascript_1.js)

---
+++ basic_javascript_projects/JS/basic_javascript_1.js
@@ -0,0 +1,8 @@
+var A = "I'm a string variable.", B = "I'm a string. ", B = B.fontcolor("red"), C = "\"Look, I'm escaping! \"", 
+C = C.fontcolor("orange"), D = "Look, " + "I'm concatenating! ", D = D.fontcolor("green");
+
+window.alert("Hello, World!");
+document.write("Hello, World! ");
+window.alert(A);
+document.write(B + C + D);
+document.write(5 + 5);
