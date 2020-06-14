f7d11b2ebc37fcd33077e624c8291826ad441810
Koddiak <lnevarez@protonmail.com>
2020-06-14 13:02:41 -0700

    Add code
    Added code to the "portfolio_website" folder.

--- portfolio_website/js/index.js
+++ portfolio_website/js/index.js
@@ -24,14 +24,31 @@ function closeForm() {
     document.getElementById("contact-form").style.display = "none";
 }
 
-// Automatic slide show functions.
-$("#slideshow > div:gt(0)").hide();
+// Slideshow.
+var slideIndex = 1;
+showSlides(slideIndex);
 
-setInterval(function() {
-    $("#slideshow > div:first")
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo("#slideshow");
-}, 3000);
+// Next/previous controls.
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("my-slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex =1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
